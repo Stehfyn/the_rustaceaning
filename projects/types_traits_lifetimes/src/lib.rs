da2b5f16d49baa60bb6a973b74e89b1fda95edf9
Stephen Foster <stephenfostercpp@gmail.com>
2023-07-19 17:53:09 -0700

    Update lib.rs

--- projects/types_traits_lifetimes/src/lib.rs
+++ projects/types_traits_lifetimes/src/lib.rs
@@ -162,6 +162,40 @@ impl<T: Display + PartialOrd> Pair<T> {
 // any runtime perf or stability cost
 
 // Lifetimes
+// &i32        // a reference
+// &'a i32     // a reference with an explicit lifetime
+// &'a mut i32 // a mutable reference with an explicit lifetime
+
+fn longest_str<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+//use std::fmt:Display
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
 
 #[cfg(test)]
 mod tests {
